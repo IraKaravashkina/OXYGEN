32b268d6e5acaa2d126f6f26dfdbb6fa62ce5448
Irina Karavashkina <dd200383karavashkina@gmail.com>
2020-06-22 14:28:56 +0300

    modif

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,25 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var pipeline = require('readable-stream').pipeline;
+var imagemin = require('gulp-imagemin');
+
+
+exports.default = () => (
+    gulp.src('src/**/*.png')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/image'))
+);
 
 gulp.task('js', function () {
     return pipeline(
-        gulp.src('src/js/app.js'),
-        uglify(),
+        gulp.src('src/main.js'),
+        //uglify(),
         gulp.dest('dist/js')
     );
 });
 
 gulp.task('scss', function () {
-    return gulp.src('src/scss/main.scss')
+    return gulp.src('src/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
 })
